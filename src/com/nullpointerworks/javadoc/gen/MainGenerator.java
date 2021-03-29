06b0cd6edf586afcb74adfeaa4b89bc740aa0e77
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-03-29 12:10:46 +0200

    Create MainGenerator.java

---
+++ src/com/nullpointerworks/javadoc/gen/MainGenerator.java
@@ -0,0 +1,20 @@
+package com.nullpointerworks.javadoc.gen;
+
+public class MainGenerator 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public MainGenerator()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	
+}
